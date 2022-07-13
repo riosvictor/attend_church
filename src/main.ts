71db4e2503bee91e798fea5834d565541037edc0
Paulo Victor Rios <victorios.ba@gmail.com>
2022-07-13 10:24:17 -0300

    schedule jobs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
+import { scheduleJob, RecurrenceRule } from 'node-schedule';
 import areaIndicators from './area_indicators'
 import wardAttendance from './wards_attendance'
- 
-// wardAttendance.start();
-areaIndicators.start();
 
+const rule = new RecurrenceRule();
+
+rule.dayOfWeek = [0, 1, 2];
+rule.hour = 22;
+rule.minute = 0;
+
+scheduleJob(rule, () => {
+  wardAttendance.start().catch(err => {
+    console.log(err);
+  });
+});
+scheduleJob(rule, () => {
+  areaIndicators.start().catch(err => {
+    console.log(err);
+  });
+}); 
