3b08acefa6d6f814b98405ec4c0918bb40f35dd1
Paulo Victor Rios <victorios.ba@gmail.com>
2022-07-20 09:05:21 -0300

    test ward att

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
-import { scheduleJob, RecurrenceRule } from 'node-schedule';
 import 'dotenv/config';
+import { scheduleJob, RecurrenceRule } from 'node-schedule';
 import areaIndicators from './area_indicators';
 import wardAttendance from './wards_attendance';
 
 const rule = new RecurrenceRule();
 
-rule.dayOfWeek = [0, 1, 2];
-rule.hour = 18;
+rule.dayOfWeek = [0, 1, 2, 3];
+rule.hour = 17;
 rule.minute = 0;
 rule.tz = 'America/Sao_Paulo'
 
 // scheduleJob(rule, () => {
+//   wardAttendance.start().catch(err => {
+//     console.log(err);
+//   });
+// });
+// scheduleJob(rule, () => {
+//   areaIndicators.start().catch(err => {
+//     console.log(err);
+//   });
+// });
+
+// console.log('Jobs are scheduled!');
+
   wardAttendance.start().catch(err => {
     console.log(err);
   });
-// });
-// scheduleJob(rule, () => {
-  areaIndicators.start().catch(err => {
-    console.log(err);
-  });
-// });
-
-console.log('Jobs are scheduled!');
