fdbc44feaf96075d9909f38982ee5a432b3d90d9
Paulo Victor Rios <victorios.ba@gmail.com>
2022-09-24 04:16:14 -0300

    Reduce day to run and change hour to 16h30

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import wardAttendance from './wards_attendance';
 
 const rule = new RecurrenceRule();
 
-rule.dayOfWeek = [0, 1, 2, 3];
-rule.hour = 17;
-rule.minute = 0;
+rule.dayOfWeek = [0, 1, 2];
+rule.hour = 16;
+rule.minute = 30;
 rule.tz = 'America/Sao_Paulo'
 
 async function run() {
